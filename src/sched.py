acbb143448ad7a26f6a10a398ec75447954d9b7d
John Hillery <John@localhost>
2022-01-14 10:31:35 -0500

    Tidy up messages

--- src/sched.py
+++ src/sched.py
@@ -48,10 +48,10 @@ class PacControl(AbstractContextManager["PacControl"]):
     NO_COURTS_MSG = "No available courts found"
     LOGIN_FORM_LOCATOR = By.CSS_SELECTOR, "form#caSignInLoginForm, form#signin_login_form"
     USERNAME_LOCATOR = By.NAME, "login"
-    RESERVE_COURT_LOCATOR_A = By.LINK_TEXT, "Reserve a Court"
+    RESERVE_LOCATOR_A = By.LINK_TEXT, "Reserve a Court"
     LOADING_SPLASH_LOCATOR = By.CSS_SELECTOR, "div#ui-id-1"
     SCHED_DATE_LOCATOR = By.CSS_SELECTOR, "input#date"
-    RESERVE_COURT_LOCATOR_B = By.CSS_SELECTOR, "a#reserve-permanent-member-button"
+    RESERVE_LOCATOR_B = By.CSS_SELECTOR, "a#reserve-permanent-member-button"
     ADD_NAME_LOCATOR = By.CSS_SELECTOR, "input#fakeUserName"
     ERROR_WIN_LOCATOR = By.CSS_SELECTOR, "div#confirm-user-popup, div#alert-dialog-1"
     RES_SUMMARY_LOCATOR = By.LINK_TEXT, "Reservation Summary"
@@ -100,9 +100,7 @@ class PacControl(AbstractContextManager["PacControl"]):
     def openBrowser(self) -> WebDriver:
         """Get web driver and open browser"""
         try:
-            crOpts = webdriver.ChromeOptions()
-            crOpts.add_experimental_option("excludeSwitches", ["enable-logging"])
-            self.webDriver = webdriver.Chrome(options=crOpts)
+            self.webDriver = webdriver.Chrome()
 
             return self.webDriver
         except WebDriverException as e:
@@ -131,7 +129,7 @@ class PacControl(AbstractContextManager["PacControl"]):
 
             doingMsg = "complete log-in"
             WebDriverWait(self.webDriver, 15).until(
-                element_to_be_clickable(PacControl.RESERVE_COURT_LOCATOR_A),
+                element_to_be_clickable(PacControl.RESERVE_LOCATOR_A),
                 "Timed out waiting to log-in")
             self.loggedIn = True
             # now on home page
@@ -173,15 +171,14 @@ class PacControl(AbstractContextManager["PacControl"]):
     def navigateToSchedule(self) -> None:
         doingMsg = "read initial schedule date"
         try:
-            self.clickAndLoad("reserve court from home page",
-                              PacControl.RESERVE_COURT_LOCATOR_A)
+            self.clickAndLoad("reserve court on home page", PacControl.RESERVE_LOCATOR_A)
 
-            schDate = self.webDriver.find_element(*PacControl.SCHED_DATE_LOCATOR) \
-                .get_attribute("value")
+            schDate = self.webDriver.find_element(
+                *PacControl.SCHED_DATE_LOCATOR).get_attribute("value")
             diff = self.requestDate - datetime.strptime(schDate, "%m/%d/%Y").date()
 
             if diff:
-                doingMsg = f"select date {self.requestDate} on schedule in {diff}"
+                doingMsg = f"request date {self.requestDate} on schedule in {diff}"
                 self.webDriver.execute_script(
                     f"calendarAddDay($('date'), {diff.days}, 'mm/dd/yyyy');")
 
@@ -189,8 +186,7 @@ class PacControl(AbstractContextManager["PacControl"]):
                 self.waitOutLoadingSplash(doingMsg)
             # end if
 
-            self.clickAndLoad("reserve court from schedule page",
-                              PacControl.RESERVE_COURT_LOCATOR_B)
+            self.clickAndLoad("reserve court on schedule page", PacControl.RESERVE_LOCATOR_B)
             self.reservationStarted = True
         except WebDriverException as e:
             raise PacException.fromXcp(doingMsg, e) from e
@@ -213,7 +209,7 @@ class PacControl(AbstractContextManager["PacControl"]):
         doingMsg = ""
         try:
             while True:
-                doingMsg = "key-in players for reservation"
+                doingMsg = "key-in player for reservation"
                 inputFld = self.webDriver.find_element(*PacControl.ADD_NAME_LOCATOR)
                 inputFld.clear()
                 inputFld.send_keys(playerName)
@@ -336,7 +332,7 @@ class PacControl(AbstractContextManager["PacControl"]):
 
 if __name__ == "__main__":
     try:
-        pacCtrl = PacControl("court6First", "time0930First", "Wed", "playWithRobin")
+        pacCtrl = PacControl("court6First", "time0930First", "Mon", "playWithRobin")
         print(pacCtrl.getReqSummary())
 
         with pacCtrl.openBrowser(), pacCtrl:
